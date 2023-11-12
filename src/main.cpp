018cf67772e40b28358267c0b2910b4e9073e48f
AsianIntel <gautam.abhyankar@gmail.com>
2023-11-12 12:54:29 -0800

    Window Initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
+#include <glad/gl.h>
+#include <glfw/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to initialize GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "Failed to initialize Glad" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* glfw_window, const int width, const int height) {
+       glViewport(0, 0, width, height);
+    });
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
